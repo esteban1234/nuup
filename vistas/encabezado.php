d772b848ea000d332846097dc36f0a02baeaa530
esteban1234 <tva_libra@hotmail.com>
2017-11-10 10:24:34 -0600

    metas

--- vistas/encabezado.php
+++ vistas/encabezado.php
@@ -2,7 +2,7 @@
 <html lang="es">
 <head>
 	<meta charset="UTF-8">
-
+	<meta name="google-site-verification" content="8tSTE446FTW96SOc7PMgZAf19n9S5fIq6XUQBls96dw" />
 	<link rel="icon" type="image/png" href="img/favicon.png" />
 	<title>ADMINISTRADORA DE SERVICIOS NUUP</title>
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
